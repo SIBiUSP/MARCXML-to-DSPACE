a734dd8b46fee2378a1b3c82bb7af1b7bae5cdd7
Tiago Murakami <trmurakami@gmail.com>
2013-12-20 15:01:51 -0200

    Inclusão inicial do arquivo .sh

---
+++ trataregistros.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/bash
+
+cd records
+csplit -k records.xml '/</record>/' {5000}
+for xxs in xx*
+do
+sed -i 's/<record>//' $xxs
+sed -i 's/<\/record>//' $xxs
+mv $xxs $xxs.xml
+mkdir $xxs
+mv $xxs.xml $xxs/
+cd $xxs
+csplit -k $xxs.xml '/</dublin_core>/' {0}
+rm $xxs.xml
+mv xx00 dublin_core.xml
+mv xx01 metadata_usp.xml
+sed -i '1c<?xml version="1.0" encoding="UTF-8"?>' *.xml
+sed -i '$a</dublin_core>' dublin_core.xml
+wget $(cat dublin_core.xml | grep -o -e 'http.*pdf')
+ls -1 | egrep -v '.xml$'  | grep -v 'contents' > contents
+cd ..
+done
